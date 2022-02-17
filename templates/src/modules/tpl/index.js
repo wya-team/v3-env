adc100441491b7262ca939994ce2f11d33cec102
蒋东 <852778359@qq.com>
2022-02-17 11:01:48 +0800

    fix: 删除多余代码

--- templates/src/modules/tpl/index.js
+++ templates/src/modules/tpl/index.js
@@ -1,13 +1,5 @@
 export const tplNavConfig = {
-	path: '/tpl/paging/basic2',
-	name: 'tpl-paging-basic2',
-	title: '',
-
-	components: [
-		() => import('./paging-basic2/index.vue'),
-		'left',
-		'top'
-	]
+	
 };
 
 export const tplConfig = [
