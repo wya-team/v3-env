007edf59db08b74a100c25e6342d89a49170ada0
Mr.Z <zrd0921@qq.com>
2022-02-17 09:30:59 +0800

    feat(templates): add service from `@wya/vcc`

--- templates/src/hooks/service/index.js
+++
@@ -1,7 +0,0 @@
-import Base from './base';
-import Store from './store';
-
-export default {
-	createStore: Store.create,
-	clear: (new Base()).clear
-};
--- templates/src/hooks/tpl.js
+++ templates/src/hooks/tpl.js
@@ -1,4 +1,4 @@
-import Service from './service';
+import { Service } from '@wya/vcc';
 
 export const useTpl = Service.createStore({
 	key: "tplData",
